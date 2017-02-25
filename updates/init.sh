02bd8d341165e4e81b549bf8127fe8045510162c
Jeremy Newhouse <jeremy.newhouse@gmail.com>
2017-02-24 23:19:55 -0600

    update

--- updates/init.sh
+++ updates/init.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-pushd /root > /dev/null
-
 if [ -d "updates" ]; then
   echo "Updates seems to be installed. Exiting."
   return 0
 fi
 
 sudo yum -y update
-
-popd > /dev/null
--- updates/setup.sh
+++ updates/setup.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
 sudo alternatives --set python /usr/bin/python2.7
+cp /root/spark-ecs/updates/hosts /etc/
